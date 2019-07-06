c27d42b3577894a2934c6d63f8d5cce7107c8aa6
Nano Tax <vladtax@itnow.com>
2019-07-06 16:00:50 -0400

    Modify: remove tests and add start/stop scripts
    Remove: test and replace with call to test.sh
    Add: docker start script
    Add: docker stop script

--- step2-docker.sh
+++ step2-docker.sh
@@ -29,49 +29,32 @@ echo "`date '+%Y-%m-%d %H:%M:%S'` | Start Docker Image build ..."
 ./buildDockerImage.sh
 echo "`date '+%Y-%m-%d %H:%M:%S'` | Done"
 
-# Start docker image
-echo "`date '+%Y-%m-%d %H:%M:%S'` | Lunching Docker Image ..."
-
+# Add start Docker shell script
+echo "#!/bin/bash
 # Starting container and exposing internal port 8080 (with --expose) and external port 8080 (with -p)
-docker run -d --expose 8080 -p 8080:8080 $1
-
-# Remember container ID
-containerId=`docker ps -l -q`
+docker run -d --name $1 --expose 8080 -p 8080:8080 $1
 
 # Wait for the srvice to start
-sleep 25
+sleep 25" > start.sh
+chmod +x start.sh
+
+# Add stop Docker shell script
+echo "#!/bin/bash
+# Stop and forcefully remove the container
+docker rm -f \`docker ps -aqf \"name=$1\"\`" > stop.sh
+chmod +x stop.sh
+
+# Start docker image
+echo "`date '+%Y-%m-%d %H:%M:%S'` | Launching Docker container"
+./start.sh
 echo "`date '+%Y-%m-%d %H:%M:%S'` | Done"
 
-# Load first test page, expecting JSON Output: {"id":1,"content":"Hello, World!"}
-expected1='{"id":1,"content":"Hello, World!"}'
-actual1=$(curl -s "http://localhost:8080/greeting")
-if [[ $actual1 = $expected1 ]]
-then
-    echo "`date '+%Y-%m-%d %H:%M:%S'` | Passed Test 1"
-else
-    echo "`date '+%Y-%m-%d %H:%M:%S'` | Failed Test 1, expected: $expected1, actual: $actual1"
-fi
-
-# Wait for 15 seconds
-sleep 15
-
-# Load second test page, expecting JSON Output: {"id":2,"content":"Hello, User!"}
-expected2='{"id":2,"content":"Hello, User!"}'
-actual2=$(curl -s "http://localhost:8080/greeting?name=User")
-if [[ $actual2 = $expected2 ]]
-then
-    echo "`date '+%Y-%m-%d %H:%M:%S'` | Passed Test 2"
-else
-    echo "`date '+%Y-%m-%d %H:%M:%S'` | Failed Test 2, expected: $expected2, actual: $actual2"
-fi
-
-# Continue running service for next 60 seconds
-sleep 60
+# Run Tests
+echo "`date '+%Y-%m-%d %H:%M:%S'` | Start Tests"
+../test.sh 
+echo "`date '+%Y-%m-%d %H:%M:%S'` | Done"
 
 # Shut down the service
-echo "`date '+%Y-%m-%d %H:%M:%S'` | Shutting down container: $containerId"
-
-# Stop and forcefully remove the container
-docker rm -f $containerId
-
+echo "`date '+%Y-%m-%d %H:%M:%S'` | Shutting down Docker container"
+./stop.sh
 echo "`date '+%Y-%m-%d %H:%M:%S'` | Done"
