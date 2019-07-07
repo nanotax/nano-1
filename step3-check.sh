53bbaf26a21895a4885103f3ef227d91457149ba
Nano Tax <vladtax@itnow.com>
2019-07-07 01:22:57 -0400

    Add: step3 script to check on all required software

---
+++ step3-check.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Check if Python3 is installed
+if type -p python3; then
+  	echo "[CHECK!] Found Python 3"
+else
+    echo "[FAILED] Python 3 is not installed"
+    echo "  Use brew: brew install python"
+fi
+
+# Check if aws cli is installed
+
+if type -p aws; then
+  	echo "[CHECK!] Found aws cli"
+else
+    echo "[FAILED] aws cli is not installed"
+    echo "  Use pip3: pip3 install awscli"
+fi
